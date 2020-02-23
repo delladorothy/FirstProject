e86e8e7f4fd9fea487e861ff5db90dad907a6867
kdw <kimdw010130@daum.net>
2020-02-23 20:43:38 +0900

    first

---
+++ Assets/Script/player_controller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_controller : MonoBehaviour
+{
+    Rigidbody2D rigid;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+                          
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("horizontal")
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+        
+    }
+}
