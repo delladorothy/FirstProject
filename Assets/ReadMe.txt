19c8d5597eeee2cef4bea66d867f1f60102681ad
kdw <kimdw010130@daum.net>
2020-02-23 23:02:55 +0900

    write player move

---
+++ Assets/ReadMe.txt
@@ -0,0 +1,12 @@
+﻿2-23
+unity 생성
+sprite test 생성
+
+// 나중에 설정 카매라에 원근법 추가 perspective
+2.1아틀란스(이미지 모음) import
+2.2 filer mode = point
+2.3 compression(압축) =none
+
+하수구 파일: Art by MrBeast CC-BY 3.0, GPL 2.0
+
+
--- Assets/Script/player_controller.cs
+++ Assets/Script/player_controller.cs
@@ -4,21 +4,27 @@ using UnityEngine;
 
 public class player_controller : MonoBehaviour
 {
+    
+    public float speed;
     Rigidbody2D rigid;
 
-
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        rigid = GetComponent<Rigidbody2D>();
-                          
+        rigid = GetComponent<Rigidbody2D>(); //rigid 컴포넌스을 받아 변수에 할당
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        float h = Input.GetAxisRaw("horizontal")
-        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
-        
+        float horiz = Input.GetAxisRaw("Horizontal"); //입력한 좌우 방향을 가져온다
+        float vert = Input.GetAxisRaw("Vertical");
+
+            rigid.velocity = new Vector2(horiz, vert) * speed;
+
+        Debug.Log("test: horiz:" + horiz); //입력한 키보드 방향 출력
+        Debug.Log("test: vert:" + vert);
+
+
     }
 }
