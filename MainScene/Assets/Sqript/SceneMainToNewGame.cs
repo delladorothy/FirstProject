afa08e9b356b57bf693d6160d2a6d7fb3c40072e
SinonHecateII <minecraft369@naver.com>
2020-02-24 19:39:09 +0900

    'FadeOut'

--- MainScene/Assets/Sqript/SceneMainToNewGame.cs
+++ MainScene/Assets/Sqript/SceneMainToNewGame.cs
@@ -5,32 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class SceneMainToNewGame : MonoBehaviour
 {
-    public UnityEngine.UI.Image fade;
-    float fades = 0.0f;
-    float time = 0;
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ()
+        if (Input.GetMouseButtonDown(0))
         {
-            time += Time.deltaTime;
-            if (fades <= 1.0f && time >= 0.1f)
-            {
-                fades += 0.05f;
-                fade.color = new Color(0, 0, 0, fades);
-                time = 0;
-            }
-            else if (fades >= 1.0f)
-            {
-                SceneManager.LoadScene("NewGame");
-                time = 0;
-            }
+            SceneManager.LoadScene("NewGame");
         }
     }
 }
