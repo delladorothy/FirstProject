ad60c43edc41e763bdcf3a0826e5cf776bbe0390
kdw <kimdw010130@daum.net>
2020-02-25 10:00:48 +0900

    player move, enemy create

---
+++ MainScene/Assets/Sqript/gameplaying/Enemy_monster.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_monster : MonoBehaviour
+{
+    Rigidbody2D rigid;
+    public int nextMove1;
+    public int nextMove2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+
+        Invoke("Think", 5); // think 함수를 5초 뒤에 호출
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigid.velocity = new Vector2(nextMove1, nextMove2);
+    }
+
+    void Think()
+    {
+        nextMove1 = Random.Range(-1, 2); //좌우
+        nextMove2 = Random.Range(-1, 2); //상하
+
+        Invoke("Think", 5);
+
+    }
+}
---
+++ MainScene/Assets/Sqript/gameplaying/player_controller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_controller : MonoBehaviour
+{
+    public float speed = 3;
+    Rigidbody2D rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float horiz = Input.GetAxisRaw("Horizontal");
+        float vert = Input.GetAxisRaw("Vertical");
+
+        rigid.velocity = new Vector2(horiz*speed, vert*speed);
+
+
+
+
+    }
+}
